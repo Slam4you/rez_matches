f597870ea8663a4d6f70189c1c495e4573e6adfb
Slam4you <zybr.k@mail.ru>
2020-11-02 16:29:24 +0300

    Initial commit

---
+++ rez.py
@@ -0,0 +1,28 @@
+dic = {}
+for i in range(int(input())):
+    string = input().split(';')
+    if string[0] not in dic:
+        dic[string[0]] = [1, 0, 0, 0, 0]  # Команда:Всего_игр, Побед, Ничьих, Поражений, Всего_очков
+    else:
+        dic[string[0]][0] += 1
+    if string[2] not in dic:
+        dic[string[2]] = [1, 0, 0, 0, 0]
+    else:
+        dic[string[2]][0] += 1
+    if int(string[1]) == int(string[3]):
+        dic[string[0]][2] += 1
+        dic[string[0]][4] += 1
+        dic[string[2]][2] += 1
+        dic[string[2]][4] += 1
+    else:
+        if int(string[1]) < int(string[3]):
+            dic[string[0]][3] += 1
+            dic[string[2]][1] += 1
+            dic[string[2]][4] += 3
+        else:
+            dic[string[2]][3] += 1
+            dic[string[0]][1] += 1
+            dic[string[0]][4] += 3
+for keys, values in dic.items():
+    print(keys + ':',end = '')
+    print(*values)
